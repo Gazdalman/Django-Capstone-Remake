f020559b72e9ed50becfa56083f5ca90c03dd2e1
Toney <ta.winston@outlook.com>
2024-05-18 11:14:46 -0500

    Added fields to reward model

--- rewards/models.py
+++ rewards/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Reward(models.Model):
+  title = models.CharField(null=False, blank=False, max_length=50)
+  amount = models.FloatField(null=False, blank=False)
